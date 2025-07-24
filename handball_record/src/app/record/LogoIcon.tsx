61f70bf54693a01592ea969fbd6a90d1eda18a4c
unknown <weilung92@gmail.com>
2025-07-25 01:57:50 +0800

    fix record/page.tsx

---
+++ handball_record/src/app/record/LogoIcon.tsx
@@ -0,0 +1,13 @@
+import {
+  IconBallFootball,
+} from "@tabler/icons-react";
+export const LogoIcon = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <IconBallFootball className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200"/>
+    </a>
+  );
+};
--- handball_record/src/app/record/page.tsx
+++ handball_record/src/app/record/page.tsx
@@ -6,7 +6,6 @@ import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 import {
   IconUserCircle,
   IconFileUpload,
-  IconBallFootball,
   IconUsersGroup,
   IconPlayHandball,
   IconLogout
@@ -16,6 +15,8 @@ import { cn } from "@/lib/utils";
 import { auth } from "@/lib/firebase";
 import { signOut } from "firebase/auth";
 import { Icon } from "lucide-react";
+import { Logo } from "@/app/record/Logo";
+import { LogoIcon } from "@/app/record/LogoIcon";
 
 interface net {
   x: number;
@@ -446,31 +447,3 @@ export default function RecordPage() {
     </div>
   );
 }
-export const Logo = () => {
-  return (
-    <a
-      href="/record"
-      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
-    >
-      {/* <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black dark:bg-white" /> */}
-      <IconBallFootball className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200"/>
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className="font-medium whitespace-pre text-black dark:text-white"
-      >
-        記錄頁面
-      </motion.span>
-    </a>
-  );
-};
-export const LogoIcon = () => {
-  return (
-    <a
-      href="#"
-      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
-    >
-      <IconBallFootball className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200"/>
-    </a>
-  );
-};
